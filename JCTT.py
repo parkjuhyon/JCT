93cbb92be1862b59bc318d746f4e0270e13549f4
parkjuhyon <106162542+parkjuhyon@users.noreply.github.com>
2025-07-09 10:46:35 +0900

    Update JCTT.py

--- JCTT.py
+++ JCTT.py
@@ -1,4 +1,4 @@
--import streamlit as st
+import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
